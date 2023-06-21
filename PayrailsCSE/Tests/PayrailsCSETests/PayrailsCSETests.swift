ba63e016ea8cfaadfeaf4b7a4dead680a97538b4
Nikita Gubchenko <nikita@payrails.com>
2023-06-21 15:56:26 +0200

    PR-2360: Create CSE package

---
+++ PayrailsCSE/Tests/PayrailsCSETests/PayrailsCSETests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import PayrailsCSE
+
+final class PayrailsCSETests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(PayrailsCSE().text, "Hello, World!")
+    }
+}
